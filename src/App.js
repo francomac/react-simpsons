582077747653c988eed3a8c4aad6699af4fe1799
Franco Carballo <franco.mcarballo@outlook.com>
2022-04-13 21:58:35 -0600

    main-page component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { MainPage } from './components/main-page';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MainPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
