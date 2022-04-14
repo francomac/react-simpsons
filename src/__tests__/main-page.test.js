20edf23a2436c3bee684d1214512b7a2c1b95d03
Franco Carballo <franco.mcarballo@outlook.com>
2022-04-13 22:39:30 -0600

    test list items from mock server

--- src/__tests__/main-page.test.js
+++ src/__tests__/main-page.test.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 
 import { MainPage } from "../components/main-page";
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const fakeQuotes = [
+  { quote: "Gah, stupid sexy Flanders!" },
+  { quote: "Eat my shorts" },
+  { quote: "Shut up, brain. I got friends now. I don't need you anymore" },
+];
+
+const server = setupServer(
+  rest.get("/quotes", (req, res, ctx) => {
+    return res(ctx.json(fakeQuotes));
+  })
+);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
 
 // eslint-disable-next-line testing-library/no-render-in-setup
 beforeEach(() => render(<MainPage />));
 
 describe("Main Page mount", () => {
-  it("must display the main page title", () => {
+  it("must display the main page title", async () => {
     expect(
       screen.getByRole("heading", { name: /simpsons quotes/i })
     ).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
   });
 });
 
@@ -23,10 +49,10 @@ describe("Quotes List", () => {
     const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole(
       "listitem"
     );
-    expect(firstQuote.textContent).toBe("Gah, stupid sexy Flanders!");
-    expect(secondQuote.textContent).toBe("Eat my shorts");
-    expect(thirdQuote.textContent).toBe(
-      "Shut up, brain. I got friends now. I don't need you anymore"
-    );
+
+    const [fakeOne, fakeTwo, fakeThird] = fakeQuotes;
+    expect(firstQuote.textContent).toBe(fakeOne.quote);
+    expect(secondQuote.textContent).toBe(fakeTwo.quote);
+    expect(thirdQuote.textContent).toBe(fakeThird.quote);
   });
 });
